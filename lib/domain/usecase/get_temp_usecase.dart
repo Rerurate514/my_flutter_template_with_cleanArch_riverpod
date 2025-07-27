e6ac8789dae19d4d3c07e0913242b37f8cc0bf04
Rerurate514 <rerurateyuto@gmail.com>
2025-07-28 07:39:15 +0900

    domainフォルダの作成

---
+++ lib/domain/usecase/get_temp_usecase.dart
@@ -0,0 +1,7 @@
+//applicationで実装するusecaseのインターフェースをここで定義
+
+// import 'package:url_shorter_by_cleanuri/domain/entity/short_url.dart';
+
+// abstract class GetShortUrlUsecase {
+//   Future<ShortUrl> execute(String url);
+// }
